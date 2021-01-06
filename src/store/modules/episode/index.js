14a65f4503a3dfcc0c84d37ff1afa1605e004150
Will1400 <44838295+Will1400@users.noreply.github.com>
2021-01-06 09:28:53 +0100

    Uncommented default episodes

--- src/store/modules/episode/index.js
+++ src/store/modules/episode/index.js
@@ -6,30 +6,30 @@ export default {
 	state() {
 		return {
 			episodes: [
-				// {
-				// 	id: "12312321e",
-				// 	title: "Klarede opvasken",
-				// 	date: "15 min siden",
-				// 	rating: 4,
-				// 	description:
-				// 		"Kunne ikke overskue at vaske op hele sidste ige. Klarede det endeligt efter af have snakket med Niels",
-				// },
-				// {
-				// 	id: "23412daswd",
-				// 	title: "Gik en lang tur",
-				// 	date: "I går kl 07.30",
-				// 	rating: 3,
-				// 	description:
-				// 		"Gik en mega lang tur i skoven. Alle bladene og de flotte farver gjorde mig varm inden i",
-				// },
-				// {
-				// 	id: "kajwhdnawu",
-				// 	title: "S-tog",
-				// 	date: "I går kl 07.30",
-				// 	rating: 1,
-				// 	description:
-				// 		"Tog toget til skole. En klam stodder der lugtede af sveskehud satte sig ved siden af mig og var totalt creepy",
-				// },
+				{
+					id: "12312321e",
+					title: "Klarede opvasken",
+					date: "15 min siden",
+					rating: 4,
+					description:
+						"Kunne ikke overskue at vaske op hele sidste ige. Klarede det endeligt efter af have snakket med Niels",
+				},
+				{
+					id: "23412daswd",
+					title: "Gik en lang tur",
+					date: "I går kl 07.30",
+					rating: 3,
+					description:
+						"Gik en mega lang tur i skoven. Alle bladene og de flotte farver gjorde mig varm inden i",
+				},
+				{
+					id: "kajwhdnawu",
+					title: "S-tog",
+					date: "I går kl 07.30",
+					rating: 1,
+					description:
+						"Tog toget til skole. En klam stodder der lugtede af sveskehud satte sig ved siden af mig og var totalt creepy",
+				},
 			],
 		};
 	},
