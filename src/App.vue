1afdedceabb2d88835c947f8ecdd6849a334a1ba
Will1400 <44838295+Will1400@users.noreply.github.com>
2021-01-07 13:24:23 +0100

    Added transition to routes

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,11 @@
 	<div class="bg-green-100 min-h-screen">
 		<the-header></the-header>
 		<div class="container lg:pt-10 m-auto relative">
-			<router-view></router-view>
+			<router-view v-slot="isProps">
+				<transition name="route" mode="out-in">
+					<component :is="isProps.Component"></component>
+				</transition>
+			</router-view>
 		</div>
 	</div>
 </template>
@@ -17,3 +21,27 @@ export default {
 	},
 };
 </script>
+
+<style scoped>
+.route-enter-from {
+	opacity: 0;
+	transform: translateY(-30px);
+}
+.route-leave-to {
+	opacity: 0;
+	transform: translateY(30px);
+}
+
+.route-enter-active {
+	transition: 0.3s ease-out;
+}
+.route-leave-active {
+	transition: 0.3s ease-in;
+}
+
+.route-enter-to,
+.route-leave-from {
+	opacity: 1;
+	transition: translateY(0);
+}
+</style>
--- src/pages/episode/EpisodeSpecific.vue
+++ src/pages/episode/EpisodeSpecific.vue
@@ -1,7 +1,35 @@
 <template>
-	<router-view></router-view>
+	<router-view v-slot="isProps">
+		<transition name="route" mode="out-in">
+			<component :is="isProps.Component"></component>
+		</transition>
+	</router-view>
 </template>
 
 <script>
 export default {};
 </script>
+
+<style scoped>
+.route-enter-from {
+	opacity: 0;
+	transform: translateY(-30px);
+}
+.route-leave-to {
+	opacity: 0;
+	transform: translateY(30px);
+}
+
+.route-enter-active {
+	transition: 0.3s ease-out;
+}
+.route-leave-active {
+	transition: 0.3s ease-in;
+}
+
+.route-enter-to,
+.route-leave-from {
+	opacity: 1;
+	transition: translateY(0);
+}
+</style>
